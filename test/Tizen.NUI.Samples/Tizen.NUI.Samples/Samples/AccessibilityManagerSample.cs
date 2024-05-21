b45abfe7dd6785df42652570abf1fc01985f28bd
Seoyeon Kim <seoyeon2.kim@samsung.com>
2024-05-21 14:58:23 +0900

    [Test] Sample for negative y coord issue
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/AccessibilityManagerSample.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/AccessibilityManagerSample.cs
@@ -3,28 +3,13 @@ using System;
 using Tizen.NUI.BaseComponents;
 using Tizen.NUI.Accessibility;
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-///                      How to turn on and test Accessibility                              ///
-/// There are two ways to turn on screen reader (TTS) in the target                         ///
-///  1) Settings > Accessibility > Screen reader > On                                       ///
-///  2) Open Terminal and use command :                                                     ///
-///      # vconftool set -f -t bool db/setting/accessibility/tts 1                          ///
-///    If you turn off screen reader, type and execute text based command below :           ///
-///      # vconftool set -f -t bool db/setting/accessibility/tts 0                          ///
-///////////////////////////////////////////////////////////////////////////////////////////////
 
 namespace Tizen.NUI.Samples
 {
+    
     public class AccessibilityManagerSample : IExample
     {
-        const string tag = "NUITEST";
-
-        const int mRow = 3;
-        const int mColumn = 3;
-        const int mContents = mRow * mColumn;
-
         Size2D windowSize;
-        TableView table;
 
         public void Activate()
         {
@@ -32,82 +17,49 @@ namespace Tizen.NUI.Samples
             window.BackgroundColor = Color.White;
             windowSize = window.Size;
 
-            // Create Table
-            table = new TableView(mRow, mColumn)
+            View pageView = new View()
+            {
+                BackgroundColor = Color.Blue,
+                PositionUsesPivotPoint = true,
+                ParentOrigin = ParentOrigin.Center,
+                PivotPoint = PivotPoint.Center,
+                Size = new Size(500,500),
+                AccessibilityHighlightable = true,
+                AccessibilityRole = Role.PushButton,
+            };
+            window.GetDefaultLayer().Add(pageView);
+
+            View deleteButtonView = new View()
             {
                 PositionUsesPivotPoint = true,
-                PivotPoint = PivotPoint.Center,
-                ParentOrigin = ParentOrigin.Center,
-                Size = new Size((float)(windowSize.Width * 0.8), (float)(windowSize.Height * 0.8)),
-                BackgroundColor = new Color(0.2f, 0.5f, 0.5f, 1.0f),
-                CellPadding = new Vector2(10, 10),
+                ParentOrigin = NUI.ParentOrigin.TopCenter,
+                PivotPoint = NUI.PivotPoint.TopCenter,
+                PositionY = -42 - 2 - 37.5f - 93,
+                Size = new Size(93, 93),
+                BackgroundImage = CommonResource.GetFHResourcePath() + "6. List/list_ic_dropdown.png",
+
+                AccessibilityHighlightable = true,
+                AccessibilityRole = Role.PushButton,
+                AccessibilityName = "Delete"
             };
-            window.GetDefaultLayer().Add(table);
+            pageView.Add(deleteButtonView);
 
-            uint exampleCount = 0;
-
-            // Fill the contents in TableView
-            for (uint row = 0; row < mRow; row++)
-            {
-                for (uint column = 0; column < mColumn; column++)
-                {
-                    TextLabel content = CreateText();
-                    content.Name = "TextLabel" + exampleCount;
-                    content.Text = "R" + row + " - C" + column;
-
-                    ///////////////////////////////////////////////////////////////////////////////////////////////
-                    ///                 How to set Accessibility attribute to components                        ///
-                    /// 1. Create AccessibilityManager.                                                         ///
-                    /// 2. Set the focus order of each view and the index of order should start 1 (one).        ///
-                    /// 3. Set the information of each view's accessibility attribute.                          ///
-                    ///    There are four attributes, which will be read by screen reader.                      ///
-                    ///    And the order of reading is : Label -> Trait -> Value (Optional) -> Hint (Optional)  ///
-                    ///////////////////////////////////////////////////////////////////////////////////////////////
-
-                    AccessibilityManager manager = AccessibilityManager.Instance;
-                    manager.SetFocusOrder(content, ++exampleCount);
-                    manager.SetAccessibilityAttribute(content,
-                                                    AccessibilityManager.AccessibilityAttribute.Label, content.Name);
-                    manager.SetAccessibilityAttribute(content,
-                                                    AccessibilityManager.AccessibilityAttribute.Trait, "Tile");
-                    manager.SetAccessibilityAttribute(content,
-                                                    AccessibilityManager.AccessibilityAttribute.Hint, "You can run this example");
-
-                    table.AddChild(content, new TableView.CellPosition(row, column));
-                }
-            }
-
-            // You can connect with various Accessibility action signals.
-            // AccessibilityManager provides functionality of setting the focus and moving forward and backward.
-            // All touch interactions for screen will work with one more finger and tap.
-
-            AccessibilityManager.Instance.FocusedViewActivated += OnFocusedView;
         }
 
-        TextLabel CreateText()
+
+        private void OnNameRequested(object sender, GetNameEventArgs e)
         {
-            TextLabel label = new TextLabel();
-            label.MultiLine = true;
-            label.HorizontalAlignment = HorizontalAlignment.Center;
-            label.VerticalAlignment = VerticalAlignment.Center;
-            label.Focusable = true;
-            return label;
+            e.Name = "DeleteButton";
         }
 
-        // Do something with activated View, for example, launching another application.
-        private void OnFocusedView(object source, AccessibilityManager.FocusedViewActivatedEventArgs args)
+        private void OnActivated(object sender, EventArgs e)
         {
-            if (args.View)
-            {
-                // Here, in the sample, if one finger double tap, the following log will shows in terminal.
-                Tizen.Log.Error(tag, "The current focused view is "+ args.View.Name +"\n");
-            }
+            Tizen.Log.Error("Seoyeon", "Activated is called !!!!!!!!");
         }
+
 
         public void Deactivate()
         {
-            AccessibilityManager.Instance.FocusedViewActivated -= OnFocusedView;
-            if(table) { table.Unparent(); }
         }
     }
 }
